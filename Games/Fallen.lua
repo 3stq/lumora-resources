f3c3d7c322d7539bf20c151625a8fca340e168ef
123 <leftstacie@gmail.com>
2026-05-08 20:28:00 +0100

    Update Fallen.lua

--- Games/Fallen.lua
+++ Games/Fallen.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3stq/athena-resources/refs/heads/main/Fonts/bypass.lua"))() -- PRELOAD
-task.wait(2)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ec815a714420bad23f67aea987903183.lua"))()
